05e6aba6ba5763d95168361c66bd5fe3d378d75a
norbibn <szandi35234@gmail.com>
2020-04-23 14:28:25 +0200

    példa el készitve

--- példa.c
+++ példa.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 // Itt definiálod elöször amit szeretnél, milyen függvényed lesz
 int osszead(int a, int b);
 
 // Ezek a függvények amiket meg kell irni alul
-int kivonas(int a , int b); 
+// ez az első lépés
+int kivonas(int a, int b);
 
-int szorzas(int a , int b); 
+int szorzas(int a, int b);
 
-int osztas(int a , int b);
+int osztas(int a, int b);
 
-int main(){
+int main()
+{
+
     // Ide jön a kód..valami
     /* Mindig a kódba ird fölé mint én most amit változtattál
     Jó lesz, jól emlékeztél, ez a függvény hivás. Viszont itt már konkrét adatok kellenek neki
@@ -18,16 +21,28 @@ int main(){
     */
     // Ebbe a 'szam' nevü változoba mentem ki a függvény értékét
     //Megtehetem, hiszen tudom, hogy ez a függvény (osszead()) egy számot ad vissza
-    int szam = osszead(3,4);
-    
+    int szam = osszead(3, 4);
+
+    printf("%d", szam);
     // És kiiratom a számot
+
+ // harmadik lépés
     printf("A megadott összeg: %d\n", szam);
-    
+
+    szam = kivonas(10, 6);
+    printf("%d\n", szam);
+
+szam = szorzas(10,5);
+printf("%d\n",szam);
+
+
     return 0;
 }
+// ********* MAIN VÉGE *************
 
 // Ez egy int függvény , tehát egy számmal fog visszatérni: return a + b
-int osszead(int a , int b) {
+int osszead(int a, int b)
+{
 
     return a + b;
 }
@@ -35,4 +50,16 @@ int osszead(int a , int b) {
 /* Az összeadás mintájára probáld megirni a szorzást, kivonást , osztást és kiiratni.
 A függvényeket felül találod hogy kell kinézzen. Te pedig itt lenn bontsd ki és ird meg, 
 majd hivd meg a main függvénybe őket és irasd ki.
+
 */
+
+// Második lépés
+
+int kivonas(int a, int b)
+{
+    return a - b;
+}
+int szorzas(int a, int b)
+{
+     return a * b;
+}
